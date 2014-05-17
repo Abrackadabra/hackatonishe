65eedd0e5612b2973dc0b19592907a95c96e5877
mekagem <mekagem@gmail.com>
2014-05-18 00:18:13 +0400

    Send and receive tests.

--- django/api/tests.py
+++ django/api/tests.py
@@ -8,9 +8,20 @@ def _register():
     return requests.get(_address + 'register')
 
 
-def _notifications(key):
-    cookie = {'key': key}
-    return requests.get(_address + 'notifications', cookies=cookie)
+def _notifications(my_key):
+    cookies = {'key': my_key}
+    return requests.get(_address + 'notifications', cookies=cookies)
+
+
+def _recv(my_key, your_key):
+    cookies = {'key': my_key}
+    return requests.get(_address + 'chat/' + your_key + '/recv', cookies=cookies)
+
+
+def _send(my_key, your_key, message):
+    cookies = {'key': my_key}
+    params = {'message': message}
+    return requests.post(_address + 'chat/' + your_key + '/send', cookies=cookies, params=params)
 
 
 class TestRegister(TestCase):
@@ -23,28 +34,23 @@ class TestRegister(TestCase):
 
 class ApiTest(TestCase):
     def setUp(self):
-        self.key = _register().text
+        self.my_key = _register().text
+        self.your_key = _register().text
         pass
 
     def test_notifications(self):
-        response = _notifications(self.key)
+        response = _notifications(self.my_key)
         self.assertIs(response.status_code, 200)
         pass
 
-
-
-
-
-# class RequestTests(TestCase):
-
-
-
-
-
-class ChatTest(TestCase):
-    def register(self):
-        # self.key = _register()
+    def test_recv(self):
+        response = _recv(self.my_key, self.your_key)
+        self.assertIs(response.status_code, 200)
         pass
 
+    def test_send(self):
+        response = _send(self.my_key, self.your_key, 'kakoi-to_pidor')
+        self.assertIs(response.status_code, 200)
+        pass
 
 # Create your tests here.
