1a67ba1374e10100e2a9b0c35fff8854cb41d005
Andrey Anurin <abra@abra0.com>
2014-05-18 00:41:06 +0400

    change urls again

--- django/api/urls.py
+++ django/api/urls.py
@@ -6,14 +6,14 @@ from api import api
 urlpatterns = patterns(
     '',
 
-    url(r'^register/', api.register),
+    url(r'^register/$', api.register),
 
-    url(r'^chat_recv/(?P<addressee>\w*)/', api.chat_recv),
-    url(r'^chat_send/(?P<addressee>\w*)/', api.chat_send),
-    url(r'^chat/(?P<addressee>\w*)/', api.chat),
+    url(r'^chat/(?P<addressee>\w*)/recv/$', api.chat_recv),
+    url(r'^chat/(?P<addressee>\w*)/send/$', api.chat_send),
+    url(r'^chat/(?P<addressee>\w*)/$', api.chat),
 
-    url(r'^notifications/', api.notifications),
-    url(r'^torrent/', api.torrent),
+    url(r'^notifications/$', api.notifications),
+    url(r'^torrent/$', api.torrent),
     url(r'^$', api.main),
 
 )
